5f1ee394b5d9633087ebed11bc437a49adf03c14
Cleyson <cleysonph@gmail.com>
2023-12-19 18:00:36 -0300

    02:05 - Tornando o ModelMapper injetável e documentando o application.properties

--- src/main/java/br/com/treinaweb/twprojects/web/positions/mappers/ModelMapperPositionMapper.java
+++ src/main/java/br/com/treinaweb/twprojects/web/positions/mappers/ModelMapperPositionMapper.java
@@ -7,16 +7,14 @@ import org.springframework.stereotype.Component;
 import br.com.treinaweb.twprojects.core.models.Position;
 import br.com.treinaweb.twprojects.web.positions.dtos.PositionForm;
 import br.com.treinaweb.twprojects.web.positions.dtos.PositionListItem;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 @ConditionalOnProperty(name = "br.com.treinaweb.twprojects.mappers.provider", havingValue = "modelmapper")
 public class ModelMapperPositionMapper implements PositionMapper {
 
     private final ModelMapper modelMapper;
-
-    public ModelMapperPositionMapper() {
-        this.modelMapper = new ModelMapper();
-    }
 
     @Override
     public PositionForm toPositionForm(Position position) {
