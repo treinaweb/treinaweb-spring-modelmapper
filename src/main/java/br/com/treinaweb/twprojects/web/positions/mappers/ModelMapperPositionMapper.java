bf5c21954b3abe7d23a1f48e2a6bac81394a40e8
Cleyson <cleysonph@gmail.com>
2023-12-19 17:41:12 -0300

    02:04 - Utilizando o ModelMapper na camada de mapeamento de Position

--- src/main/java/br/com/treinaweb/twprojects/web/positions/mappers/ModelMapperPositionMapper.java
+++ src/main/java/br/com/treinaweb/twprojects/web/positions/mappers/ModelMapperPositionMapper.java
@@ -1,5 +1,6 @@
 package br.com.treinaweb.twprojects.web.positions.mappers;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
@@ -8,30 +9,28 @@ import br.com.treinaweb.twprojects.web.positions.dtos.PositionForm;
 import br.com.treinaweb.twprojects.web.positions.dtos.PositionListItem;
 
 @Component
-@ConditionalOnProperty(name = "br.com.treinaweb.twprojects.mappers.provider", havingValue = "local")
-public class PositionMapperImpl implements PositionMapper {
+@ConditionalOnProperty(name = "br.com.treinaweb.twprojects.mappers.provider", havingValue = "modelmapper")
+public class ModelMapperPositionMapper implements PositionMapper {
+
+    private final ModelMapper modelMapper;
+
+    public ModelMapperPositionMapper() {
+        this.modelMapper = new ModelMapper();
+    }
 
     @Override
     public PositionForm toPositionForm(Position position) {
-        return PositionForm.builder()
-            .name(position.getName())
-            .build();
+        return modelMapper.map(position, PositionForm.class);
     }
 
     @Override
     public Position toPosition(PositionForm positionForm) {
-        return Position.builder()
-            .name(positionForm.getName())
-            .build();
+        return modelMapper.map(positionForm, Position.class);
     }
 
     @Override
     public PositionListItem toPositionListItem(Position position) {
-        System.out.println("Local");
-        return PositionListItem.builder()
-            .id(position.getId())
-            .name(position.getName())
-            .build();
+        return modelMapper.map(position, PositionListItem.class);
     }
     
 }
